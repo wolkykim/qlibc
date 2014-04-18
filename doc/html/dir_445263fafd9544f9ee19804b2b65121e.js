138441c90543570713d0343dd893208b4f902517
Snow Kim <wolkykim@gmail.com>
2014-04-18 07:25:54 +0000

    Update doc.

--- doc/html/dir_445263fafd9544f9ee19804b2b65121e.js
+++ doc/html/dir_445263fafd9544f9ee19804b2b65121e.js
@@ -4,5 +4,6 @@ var dir_445263fafd9544f9ee19804b2b65121e =
     [ "qconfig.c", "qconfig_8c.html", "qconfig_8c" ],
     [ "qdatabase.c", "qdatabase_8c.html", "qdatabase_8c" ],
     [ "qhttpclient.c", "qhttpclient_8c.html", "qhttpclient_8c" ],
-    [ "qlog.c", "qlog_8c.html", "qlog_8c" ]
+    [ "qlog.c", "qlog_8c.html", "qlog_8c" ],
+    [ "qtokenbucket.c", "qtokenbucket_8c_source.html", null ]
 ];
