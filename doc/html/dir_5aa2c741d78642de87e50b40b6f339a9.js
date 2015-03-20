5c4593db65846d9a8e23268736cd58e26a2c125f
Seungyoung "Steve" Kim <wolkykim@gmail.com>
2015-03-20 12:28:13 -0700

    doc update

--- doc/html/dir_5aa2c741d78642de87e50b40b6f339a9.js
+++ doc/html/dir_5aa2c741d78642de87e50b40b6f339a9.js
@@ -6,5 +6,6 @@ var dir_5aa2c741d78642de87e50b40b6f339a9 =
     [ "qlisttbl.c", "qlisttbl_8c.html", "qlisttbl_8c" ],
     [ "qqueue.c", "qqueue_8c.html", "qqueue_8c" ],
     [ "qstack.c", "qstack_8c.html", "qstack_8c" ],
+    [ "qtreetbl.c", "qtreetbl_8c.html", "qtreetbl_8c" ],
     [ "qvector.c", "qvector_8c.html", "qvector_8c" ]
 ];
