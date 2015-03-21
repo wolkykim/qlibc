9899a35e147ab866dc7f1696b82436122b5d4d9e
Seungyoung "Steve" Kim <wolkykim@gmail.com>
2015-03-21 07:18:58 -0700

    Add unit test result aggregator script.
    ======================================================================
    ****           Good job! All tests were successful                ****
    ======================================================================
    *   ____                 _       _    All tests were all successful. *
    *  / ___| ___   ___   __| |     | | ___ | |__   | |                  *
    * | |  _ / _ \ / _ \ / _` |  _  | |/ _ \| '_ \  | |                  *
    * | |_| | (_) | (_) | (_| | | |_| | (_) | |_) | |_|                  *
    *  \____|\___/ \___/ \__,_|  \___/ \___/|_.__/  (_)                  *
    ======================================================================
    Tested: test_qhashtbl test_qhasharr test_qhasharr_darkdh test_qtreetbl test_qstring

---
+++ tests/launcher.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ $# = 0 ]; then
+	echo "This program is called by make. Please use \"make test\" command instead."
+	exit 1
+fi
+
+FAIL=0
+FAILDESC=""
+for EXECUTABLE in $*; do
+	./$EXECUTABLE
+	if [ $? != 0 ]; then
+		FAIL=1
+		FAILDESC="$FAILDESC $EXECUTABLE"
+	fi
+done
+
+echo""
+if [ $FAIL != 0 ]; then
+	echo "======================================================================"
+	echo "**** OOOOOPS!!! UNSUCESSFUL UNIT TEST FOUND. PLEASE FIX AND RERUN ****"
+	echo "======================================================================"
+	echo "Fails in =>$FAILDESC"
+	exit 1
+fi
+
+echo "======================================================================"
+echo "****           Good job! All tests were successful                ****"
+echo "======================================================================"
+echo "*   ____                 _       _    All tests were all successful. *"  
+echo "*  / ___| ___   ___   __| |     | | ___ | |__   | |                  *"
+echo "* | |  _ / _ \ / _ \ / _\` |  _  | |/ _ \| '_ \  | |                  *"
+echo "* | |_| | (_) | (_) | (_| | | |_| | (_) | |_) | |_|                  *"
+echo "*  \____|\___/ \___/ \__,_|  \___/ \___/|_.__/  (_)                  *"
+echo "======================================================================"
+echo "Tested: $*"
+exit 0
