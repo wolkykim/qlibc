8e96e4367e7a5a338b06553ead72a425619b38d1
Seungyoung "Steve" Kim <wolkykim@gmail.com>
2015-03-24 23:37:00 -0700

    Support traditional style API for qhashtbl

--- include/qlibc/containers/qhashtbl.h
+++ include/qlibc/containers/qhashtbl.h
@@ -47,12 +47,39 @@ extern "C" {
 typedef struct qhashtbl_s qhashtbl_t;
 typedef struct qhashtbl_obj_s qhashtbl_obj_t;
 
-/* public functions */
 enum {
     QHASHTBL_THREADSAFE = (0x01)  /*!< make it thread-safe */
 };
 
+/* member functions
+ *
+ * All the member functions can be accessed in both ways:
+ *  - tbl->put(tbl, ...);      // easier to switch the container type to other kinds.
+ *  - qhashtbl_put(tbl, ...);  // where avoiding pointer overhead is preferred.
+ */
 extern qhashtbl_t *qhashtbl(size_t range, int options);  /*!< qhashtbl constructor */
+
+extern bool qhashtbl_put(qhashtbl_t *tbl, const char *name, const void *data, size_t size);
+extern bool qhashtbl_putstr(qhashtbl_t *tbl, const char *name, const char *str);
+extern bool qhashtbl_putstrf(qhashtbl_t *tbl, const char *name, const char *format, ...);
+extern bool qhashtbl_putint(qhashtbl_t *tbl, const char *name, int64_t num);
+
+extern void *qhashtbl_get(qhashtbl_t *tbl, const char *name, size_t *size, bool newmem);
+extern char *qhashtbl_getstr(qhashtbl_t *tbl, const char *name, bool newmem);
+extern int64_t qhashtbl_getint(qhashtbl_t *tbl, const char *name);
+
+extern bool qhashtbl_remove(qhashtbl_t *tbl, const char *name);
+
+extern bool qhashtbl_getnext(qhashtbl_t *tbl, qhashtbl_obj_t *obj, bool newmem);
+
+extern size_t qhashtbl_size(qhashtbl_t *tbl);
+extern void qhashtbl_clear(qhashtbl_t *tbl);
+extern bool qhashtbl_debug(qhashtbl_t *tbl, FILE *out);
+
+extern void qhashtbl_lock(qhashtbl_t *tbl);
+extern void qhashtbl_unlock(qhashtbl_t *tbl);
+
+extern void qhashtbl_free(qhashtbl_t *tbl);
 
 /**
  * qhashtbl container object structure
