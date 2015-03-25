19e50a255713abb533aff2e41d8e5b83c6e98c0a
Seungyoung "Steve" Kim <wolkykim@gmail.com>
2015-03-25 00:33:24 -0700

    Issue #23: Update doc for supporting traditional style APIs

--- doc/html/qvector_8c.js
+++ doc/html/qvector_8c.js
@@ -1,14 +1,14 @@
 var qvector_8c =
 [
     [ "qvector", "qvector_8c.html#a203044eab6c840e593223148b24e8995", null ],
-    [ "add", "qvector_8c.html#abbe3fb852d781b23a1fe83640b21f56d", null ],
-    [ "addstr", "qvector_8c.html#ad5764907651b58d1ec8d6c931db55ec5", null ],
-    [ "addstrf", "qvector_8c.html#aff9535368a548912d8b217b3294c1ade", null ],
-    [ "toarray", "qvector_8c.html#a30a79c3af88d8a0876ccb4d494f8d429", null ],
-    [ "tostring", "qvector_8c.html#a3d95ee6881be10a3df2e09eaf36c0f24", null ],
-    [ "size", "qvector_8c.html#a78f645330013cc18a537afc861b13907", null ],
-    [ "datasize", "qvector_8c.html#aefd06548bebd04af5a005cb7f2e3e45f", null ],
-    [ "clear", "qvector_8c.html#a550980052249647bc46e121e645b97c3", null ],
-    [ "debug", "qvector_8c.html#a3a7f49d20b2d2dc4abcfb34ac41a0b61", null ],
-    [ "free_", "qvector_8c.html#abc7bba00f37d1569db118c4b9bcad9d5", null ]
+    [ "qvector_add", "qvector_8c.html#a1c334529ab20886033a64413a17760d1", null ],
+    [ "qvector_addstr", "qvector_8c.html#a39902c2ec296e42892b71058bff7c1fd", null ],
+    [ "qvector_addstrf", "qvector_8c.html#a17a159b4acbfbf97bc106798ac92eaff", null ],
+    [ "qvector_size", "qvector_8c.html#aea2dd45d8692621f3aba1822b8f9e80e", null ],
+    [ "qvector_datasize", "qvector_8c.html#a550eb3c6d77ad7f2f6c38b3ed05c0dfa", null ],
+    [ "qvector_toarray", "qvector_8c.html#ae367e5e5fc33e2eb5dc9eb8791efcb15", null ],
+    [ "qvector_tostring", "qvector_8c.html#a31171fcb6798dc2886f12e06ce5313be", null ],
+    [ "qvector_clear", "qvector_8c.html#a6aa6d22249f4cbeff4cbcb31aea99063", null ],
+    [ "qvector_debug", "qvector_8c.html#a217d5fe3cb59560703d84e47097dc989", null ],
+    [ "qvector_free", "qvector_8c.html#a69479bf55198bdd1c29e906d1b069480", null ]
 ];
