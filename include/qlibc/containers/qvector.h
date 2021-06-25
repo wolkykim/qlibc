8f6644eb2ff529593e01a6f3b41efce34c4ff429
Aragami1408 <vucaominh1408@gmail.com>
2021-06-25 17:13:19 +0700

    fixed qvector_t indent

--- include/qlibc/containers/qvector.h
+++ include/qlibc/containers/qvector.h
@@ -53,10 +53,10 @@ typedef struct qvector_obj_s qvector_obj_t;
 
 /* public functions */
 enum {
-    QVECTOR_THREADSAFE = (0x01),  /*!< make it thread-safe */
-    QVECTOR_RESIZE_DOUBLE = (0x02), /*!< double the size when vector is full*/
-    QVECTOR_RESIZE_LINEAR = (0x04), /*!< add the size with initial num when vector is full*/
-    QVECTOR_RESIZE_EXACT = (0x08) /*!< add up as much as needed*/
+   QVECTOR_THREADSAFE = (0x01),  /*!< make it thread-safe */
+   QVECTOR_RESIZE_DOUBLE = (0x02), /*!< double the size when vector is full*/
+   QVECTOR_RESIZE_LINEAR = (0x04), /*!< add the size with initial num when vector is full*/
+   QVECTOR_RESIZE_EXACT = (0x08) /*!< add up as much as needed*/
 };
 
 extern qvector_t *qvector(size_t max, size_t objsize, int options);
@@ -100,56 +100,56 @@ extern bool qvector_getnext(qvector_t *vector, qvector_obj_t *obj, bool newmem);
  * qvector container object
  */
 struct qvector_s {
-    /* capsulated member functions */
+   /* capsulated member functions */
 
-    bool (*addfirst)(qvector_t *vector, const void *object);
-    bool (*addlast)(qvector_t *vector, const void *data);
-    bool (*addat)(qvector_t *vector, int index, const void *data);
+   bool (*addfirst)(qvector_t *vector, const void *object);
+   bool (*addlast)(qvector_t *vector, const void *data);
+   bool (*addat)(qvector_t *vector, int index, const void *data);
 
-    void *(*getfirst)(qvector_t *vector, bool newmem);
-    void *(*getlast)(qvector_t *vector, bool newmem);
-    void *(*getat)(qvector_t *vector, int index, bool newmem);
+   void *(*getfirst)(qvector_t *vector, bool newmem);
+   void *(*getlast)(qvector_t *vector, bool newmem);
+   void *(*getat)(qvector_t *vector, int index, bool newmem);
 
-    bool (*setfirst)(qvector_t *vector, const void *data);
-    bool (*setlast)(qvector_t *vector, const void *data);
-    bool (*setat)(qvector_t *vector, int index, const void *data);
+   bool (*setfirst)(qvector_t *vector, const void *data);
+   bool (*setlast)(qvector_t *vector, const void *data);
+   bool (*setat)(qvector_t *vector, int index, const void *data);
 
-    void *(*popfirst)(qvector_t *vector);
-    void *(*poplast)(qvector_t *vector);
-    void *(*popat)(qvector_t *vector, int index);
+   void *(*popfirst)(qvector_t *vector);
+   void *(*poplast)(qvector_t *vector);
+   void *(*popat)(qvector_t *vector, int index);
 
-    bool (*removefirst)(qvector_t *vector);
-    bool (*removelast)(qvector_t *vector);
-    bool (*removeat)(qvector_t *vector, int index);
+   bool (*removefirst)(qvector_t *vector);
+   bool (*removelast)(qvector_t *vector);
+   bool (*removeat)(qvector_t *vector, int index);
 
-    size_t (*size)(qvector_t *vector);
-    bool   (*resize)(qvector_t *vector, size_t newmax);
+   size_t (*size)(qvector_t *vector);
+   bool   (*resize)(qvector_t *vector, size_t newmax);
 
-    void *(*toarray)(qvector_t *vector, size_t *size);
+   void *(*toarray)(qvector_t *vector, size_t *size);
 
-    void (*lock)(qvector_t *vector);
-    void (*unlock)(qvector_t *vector);
+   void (*lock)(qvector_t *vector);
+   void (*unlock)(qvector_t *vector);
 
-    void (*clear)(qvector_t *vector);
-    bool (*debug)(qvector_t *vector, FILE *out);
-    void (*free)(qvector_t *vector);
+   void (*clear)(qvector_t *vector);
+   bool (*debug)(qvector_t *vector, FILE *out);
+   void (*free)(qvector_t *vector);
 
-    void (*reverse)(qvector_t *vector);
-    bool (*getnext)(qvector_t *vector, qvector_obj_t *obj, bool newmem);
+   void (*reverse)(qvector_t *vector);
+   bool (*getnext)(qvector_t *vector, qvector_obj_t *obj, bool newmem);
 
-    /* private variables - do not access directly */
-    void *qmutex;
-    void *data;
-    size_t num; /*number of elements*/
-    size_t objsize; /*the size of each element*/
-    size_t max; /*allocated number of elements*/
-    int options;
-    size_t initnum;
+   /* private variables - do not access directly */
+   void *qmutex;
+   void *data;
+   size_t num; /*number of elements*/
+   size_t objsize; /*the size of each element*/
+   size_t max; /*allocated number of elements*/
+   int options;
+   size_t initnum;
 };
 
 struct qvector_obj_s { 
-    void *data;
-    int index;
+   void *data;
+   int index;
 };
 
 #ifdef __cplusplus
