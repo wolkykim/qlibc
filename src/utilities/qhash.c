d7271411bb57dca5e0755ccbcf395f4763c9d09f
Brett Kuskie <fullaxx@gmail.com>
2022-10-26 11:51:16 -0600

    Fix indentation

--- src/utilities/qhash.c
+++ src/utilities/qhash.c
@@ -125,11 +125,11 @@ bool qhashmd5_file(const char *filepath, off_t offset, ssize_t nbytes,
         }
     }
 
-	// If we are requested to read the entire file,
-	// Then set nbytes to size, to properly execute the for loop below
-	if(nbytes == 0) {
-		nbytes = size;
-	}
+    // If we are requested to read the entire file,
+    // Then set nbytes to size, to properly execute the for loop below
+    if(nbytes == 0) {
+        nbytes = size;
+    }
 
     MD5_CTX context;
     MD5Init(&context);
