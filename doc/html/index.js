544592e97c1a558658cc4584c37823c6d11fb979
Steve Kim <wolkykim@gmail.com>
2023-07-26 17:09:38 -0700

    regenerate docs

---
+++ doc/html/index.js
@@ -0,0 +1,9 @@
+var index =
+[
+    [ "qLibc Copyright", "index.html#autotoc_md0", null ],
+    [ "API Reference", "index.html#autotoc_md1", null ],
+    [ "qLibc Tables at a Glance", "index.html#autotoc_md2", null ],
+    [ "Consistent API Look", "index.html#autotoc_md3", null ],
+    [ "Looking for people to work with.", "index.html#autotoc_md4", null ],
+    [ "Contributors", "index.html#autotoc_md5", null ]
+];
