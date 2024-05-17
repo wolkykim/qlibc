b97c5813c439dca46a825f3ef3f15e17fcbef4b5
SunBeau <SunBeau@163.com>
2024-05-17 09:33:34 +0800

    merge qlibpq.h into qdatabase.c

--- src/extensions/qdatabase.c
+++ src/extensions/qdatabase.c
@@ -89,7 +89,157 @@
 #endif /* ENABLE_MYSQL */
 
 #ifdef ENABLE_PGSQL
-#include "qlibpq.h"
+#include "libpq-fe.h"
+
+/*
+Wrap the low-level functions of the libpq library.
+Provide interfaces with naming convention similar to mysql_xyz().
+*/
+
+#define qtype_cast(_type, _src) ((_type)_src)
+
+typedef enum {
+    PQ_UNKNOWN,
+    PQ_WRITE,
+    PQ_READ,
+    PQ_PING,
+} pgquery_t;
+
+typedef struct {
+    char   *emsg;
+    PGconn *pgconn;
+
+    PGresult *pgresult;
+    int rows;
+    int cols;
+    int cursor;
+} pgsql_t;
+
+static inline pgsql_t* pgsql_init(void)
+{
+    pgsql_t* pgsql = (pgsql_t *)calloc(1, sizeof(pgsql_t));
+    return pgsql;
+}
+
+static inline const char* pgsql_set_emsg(pgsql_t* pgsql, const char* emsg)
+{
+    if (pgsql == NULL || emsg == NULL) {
+        return NULL;
+    }
+
+    if (pgsql->emsg) {
+        free(pgsql->emsg);
+    }
+
+    pgsql->emsg = strdup(emsg);
+    return pgsql->emsg;
+}
+
+static inline void pgsql_close(pgsql_t* pgsql)
+{
+    if (pgsql == NULL) {
+        return;
+    }
+
+    if (pgsql->emsg) {
+        free(pgsql->emsg);
+        pgsql->emsg = NULL;
+    }
+
+    if (pgsql->pgresult) {
+        PQclear(pgsql->pgresult);
+        pgsql->pgresult = NULL;
+    }
+
+    if (pgsql->pgconn) {
+        PQfinish(pgsql->pgconn);
+        pgsql->pgconn = NULL;
+    }
+
+    free(pgsql);
+}
+
+static inline void pgsql_query_result(pgsql_t* pgsql)
+{
+    if (pgsql->pgresult) {
+        PQclear(pgsql->pgresult);
+        pgsql->pgresult = NULL;
+    }
+}
+
+static inline int pgsql_query(pgsql_t* pgsql, const char *query, pgquery_t type)
+{
+    if (pgsql == NULL || pgsql->pgconn == NULL) {
+        return -1; /* error */
+    }
+
+    /* free previous query results */
+    pgsql_query_result(pgsql);
+
+    if (type == PQ_PING) {
+        query = "";
+    }
+
+    PGresult *result = PQexec(pgsql->pgconn, query);
+    ExecStatusType status = PQresultStatus(result);
+
+    switch (type) {
+        case PQ_PING: {
+            if (status != PGRES_EMPTY_QUERY) {
+                return -1; /* error */
+            }
+            break;
+        }
+
+        case PQ_READ: {
+            if (status != PGRES_TUPLES_OK) {
+                return -1; /* error */
+            }
+            break;
+        }
+
+        default: {
+            if (status != PGRES_COMMAND_OK) {
+                return -1; /* error */
+            }
+            break;
+        }
+    }
+
+    pgsql->pgresult = result;
+
+    return 0; /* ok */
+}
+
+static inline int pgsql_affected_rows(pgsql_t* pgsql)
+{
+    if (pgsql && pgsql->pgresult) {
+        return atoi(PQcmdTuples(pgsql->pgresult));
+    }
+
+    /* error */
+    return -1;
+}
+
+static inline int pgsql_num_rows(pgsql_t* pgsql)
+{
+    if (pgsql && pgsql->pgresult) {
+        return PQntuples(pgsql->pgresult);
+    }
+
+    /* error */
+    return -1;
+}
+
+static inline int pgsql_num_fields(pgsql_t* pgsql)
+{
+    if (pgsql && pgsql->pgresult) {
+        return PQnfields(pgsql->pgresult);
+    }
+
+    /* error */
+    return -1;
+}
 #endif /* ENABLE_PGSQL */
 
 #include <stdio.h>
